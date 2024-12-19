5e2ba50b6cbef860c22bb03b89f8ae1c6cd7f0c8
Sanjayvarshith <sanjayvarshith21@gmail.com>
2024-12-19 20:22:28 +0530

    smells

---
+++ backend/app.py
@@ -0,0 +1,152 @@
+# @Date: 2023-02-28
+# @Title: The core backend Flask code that runs the server, communicates with the frontend using flask_cors, and calls the algorithms.
+
+# pip install numpy panda seaborn matplotlib flask flask_cors
+from algorithm.sp_missingvalues import *
+from algorithm.correlated import *
+from algorithm.outliers import *
+from algorithm.duplicates import *
+from algorithm.imbalance import *
+from algorithm.stringsmells import *
+import json
+
+# Datasets/concrete.csv
+df = pd.read_csv('Datasets/concrete.csv')
+
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import pandas as pd, numpy as np, io, base64, matplotlib.pyplot as plt, seaborn as sns
+
+# Initialize the Flask application
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
+
+
+# route http posts to this method
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    global df
+    df = pd.read_csv(file)
+    results = {}
+    results['num_rows'] = len(df)
+    results['num_cols'] = len(df.columns)
+    results['column_names'] = list(df.columns)
+    spd = SpecialMissingValues(df)
+    results['sp_missing_values'] = {'Info': spd[0], 'InfoNan': spd[1], 'Code': spd[2], 'Code_Nan':spd[3],'splmissCols':spd[4],'missingPer':spd[5]}
+    spd = missing_values(df)
+    results['missing_values'] = {'Info': spd[0],  'Code': spd[1],'missCols':spd[2], 'missPer':spd[3]}
+    results['heatmap'] = generate_heatmap(df)
+    results['correlated'] = correlated(df)
+    results['bargraph_miss'] = generate_bargraph_missing_values(df)
+    results['bargraph_sp_miss'] = generate_bargraph_special_missing_values(df)
+    results['bargraph_nan'] = generate_bargraph_nan_values(df)
+    results['duplicates'] = duplicated(df)
+    bnc = binning_cat(df)
+    results['binning_cat'] = {'Info': bnc[0],  'Code': bnc[1], 'binCols': bnc[2], 'unqVals':bnc[3], 'plot': generate_bargraph_binning_cat(df)}
+    imb = class_imbal(df)
+    results['imbalance'] = {'Info': imb[0] + imb[1], 'imbCols': imb[2], 'imbRatio':imb[3],  'plot': generate_bargraph_class_imbal(df)}
+    # Trailng Spaces, Special Characters, Human Friendly
+    spcr = detect_special_characters(df)
+    results['sp_char'] = {'Info': spcr[0], 'Code': spcr[1], 'plot': generate_bargraph_special_characters(df)}
+    trsp = trailing_spaces(df)
+    results['tr_spaces'] = {'Info': trsp[0], 'Code': trsp[1], 'plot': generate_bargraph_trailing_spaces(df)}
+    humf = human_friendly(df)
+    results['hum_friendly'] = {'Info': humf[0], 'Code': humf[1], 'plot': generate_bargraph_human_friendly(df)}
+    outl = Outliers(df)
+    results['outliers'] = {'Info': outl[0], 'Suggestion': outl[1], 'Code': outl[2], 'plot': generate_boxplot(df)}
+    j = jsonify(results)
+    # print(j)    
+    return j
+
+
+
+# @Use: Converts Excel Column Number to Column Name
+def excelColnoToColNo(cn:str) :
+    if type(cn)== int or cn.isdigit():
+        cn = int(cn)
+        if cn < 1:
+            return 1
+        return cn
+    cn = cn.upper()
+    for i in range(len(cn)):
+        if not (ord(cn[i]) >= 65 and ord(cn[i]) <= 90):
+            return -1
+    if len(cn) == 1:
+        # A->1
+        return ord(cn) - 64
+    elif len(cn) == 2:
+        # AA->27
+        return (ord(cn[0]) - 64) * 26 + (ord(cn[1]) - 64)
+    
+    elif len(cn) == 3:
+        # AAA->703
+        return (ord(cn[0]) - 64) * 26 * 26 + (ord(cn[1]) - 64) * 26 + (ord(cn[2]) - 64)
+    else:
+        ans = 0
+        for i in range(len(cn)):
+            ans += (ord(cn[i]) - 64) * 26 ** (len(cn) - i - 1)
+        return ans
+    
+@app.route('/regularExp', methods=['POST'])
+# @Use: Custom Data Smell Detection
+def regularExp():
+    global df
+    #print(df)
+    count = 0
+    # initialize prefetching,
+    colNo = 1
+    regex = request.json['regex']
+    colNo = request.json['colNo']
+
+
+    colNo = excelColnoToColNo(colNo)
+    #string to regex string
+    #filtered_df = df[int(colNo)].str.match[(regex)]
+    #print(filtered_df)
+    # matches = df[col].astype(str).str.match(pattern)
+    # Verify that the column is of type object, and colNo is an integer less than the number of columns
+    # if df[df.columns[int(colNo)]].dtype == 'object' and int(colNo) < len(df.columns):
+    if 0 < int(colNo) < len(df.columns)+1:
+            
+        matches  = df[df.columns[int(colNo)-1]].astype(str).str.match(regex)
+        # print lentgh of matches
+        #count where matches is true
+        count = len(matches[matches == True].index)
+        print(count, len(matches))
+        percent__ = (count/len(matches)).__round__(5)*100
+        colName = df.columns[int(colNo)-1]
+        percent_ ='Column Name : '+ colName + f'\nPercentage of matches: {percent__}%'
+        
+        #print(matches)
+        print(regex)
+        print(colNo)
+        print(count)
+    else:
+        count = 0
+        percent__ = 0
+        percent_ = f'Please enter a valid column number. It should be less than {len(df.columns)+1} and more than 0.\n Indexing starts from 1.'
+    
+    return jsonify({'count': count, 'percent': percent_})
+if __name__ == '__main__':
+    app.run(debug= True)
+
+'''
+# define the regular expression to match
+regex_pattern = r'^[a-z]{3}[0-9]{2}$'
+
+col_name = 'Custom Name'
+# # filter the DataFrame using the regular expression
+filtered_df = df[col_name].str.match(regex_pattern)]
+
+# for col in filtered_df.columns:
+#     filtered_df = filtered_df[filtered_df[col].str.match(regex_pattern)]
+
+# print the filtered DataFrame
+print(filtered_df)
+
+# print the filtered DataFrame
+print(filtered_df)
+
+'''
---
+++ frontend/src/components/DefaultPage/Mainpage.jsx
@@ -0,0 +1,413 @@
+import Excel from "../ExcelSheet/Excel";
+// import { Spinner } from "react-bootstrap";
+import React, { useState } from "react";
+import Papa from "papaparse";
+import axios from "axios";
+import "./Mainpage.css";
+import jsPDF from "jspdf";
+import { Button } from "react-bootstrap";
+import html2canvas from "html2canvas";
+import Codebox from "../Codebox";
+import Table from "./Table";
+import { RegExForm } from "../RegularExpression/form";
+
+const language = "python";
+export default function MainPage() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [jsonData, setJsonData] = useState(null);
+  const [analysisData, setAnalysisData] = useState(null);
+  const [heatmapData, setHeatmapData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [bargraph_sp_miss, setBargraph_sp_miss] = useState(null);
+  const [bargraph_nan, setBargraph_nan] = useState(null);
+  const [bargraph_binning_cat, setBargraph_binning_cat] = useState(null);
+  const [bargraph_class_imbal, setBargraph_class_imbal] = useState(null);
+  // const [bargraph_sp_char, setBargraph_sp_char] = useState(null);
+  // const [bargraph_hum_friendly, setBargraph_hum_friendly] = useState(null);
+  // const [bargraph_tr_spaces, setBargraph_tr_spaces] = useState(null);
+  const bargraph_sp_char = null;
+  const bargraph_hum_friendly = null;
+  const bargraph_tr_spaces = null;
+  const [boxplot, setBoxplot] = useState(null);
+  const [click, setClick] = useState(false);
+  const [fileChosen, setFileChosen] = useState(false);
+
+  const handleFileUpload = (event) => {
+    setFileChosen(true);
+    setSelectedFile(event.target.files[0]);
+    // Added for excel Purpose, which itself is Okay
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+      complete: function (result) {
+        setJsonData(result.data);
+      },
+    });
+  };
+
+  const handleUpload = () => {
+    setClick(true);
+    setIsLoading(true); // we will set this to false when the response is received
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    axios
+      .post("http://127.0.0.1:5000/upload", formData)
+      .then((response) => {
+        setAnalysisData(response.data);
+        setHeatmapData(response.data.heatmap);
+        setBargraph_sp_miss(response.data.bargraph_sp_miss);
+        setBargraph_nan(response.data.bargraph_nan);
+        setBoxplot(response.data.outliers.plot);
+        setBargraph_binning_cat(response.data.binning_cat.plot);
+        setBargraph_class_imbal(response.data.imbalance.plot);
+        // setBargraph_sp_char(response.data.sp_char.plot);
+        // setBargraph_hum_friendly(response.data.hum_friendly.plot);
+        // setBargraph_tr_spaces(response.data.tr_spaces.plot);
+        console.log(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  };
+  const handleDownload = () => {
+    html2canvas(document.body).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "PNG", 0, 0);
+      pdf.save("page-content.pdf");
+    });
+  };
+
+  // const handleDownload = () => {
+  //   setIsLoading(true); // off it when the pdf is downloaded, i.e after the pdf is saved
+  //   const pdf = new jsPDF();
+  //   const resultsContainer = document.getElementById("results-container");
+  //   pdf.fromHTML(resultsContainer, 15, 15);
+  //   // pdf.save('results.pdf');
+  //   // setIsLoading(false);
+  //   // it should be asynchoronous
+  //   pdf
+  //     .save("results.pdf")
+  //     .then(() => {
+  //       setIsLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       setIsLoading(false);
+  //     });
+  // };
+
+  return (
+    <div className="App">
+      {isLoading && (
+        <div className="loading-overlay">
+          <div className="spinner"></div>
+        </div>
+      )}
+
+      <div className="heading-text">
+        <h1>SniffCSV DataSmell Detector</h1>
+      </div>
+      <div className="Intput-and-Btn">
+        <br />
+        <input
+          type="file"
+          name="file"
+          accept=".csv"
+          // Removed the onChange event handler of csv file
+          onChange={handleFileUpload}
+          className="input-file"
+        />
+        <Button
+          className="analyze-btn d-flex align-self-center"
+          variant="success"
+          size="lg"
+          onClick={handleUpload}
+        >
+          Analysis
+        </Button>
+      </div>
+
+      {analysisData && (
+        <div className="analysis-container">
+          <Excel myjson={jsonData} />
+          <div className="results-container">
+            <h1 className="analysis-title">Analysis Results</h1>
+            <div className="analysis-summary">
+              <p className="head-text">
+                Number of Rows: {analysisData.num_rows}
+              </p>
+              <p className="head-text">
+                Number of Columns: {analysisData.num_cols}
+              </p>
+              {
+                <p className="head-text">
+                  Column Names:{" "}
+                  <p id="Column-Names">
+                    {analysisData.column_names.join(", ")}
+                  </p>
+                </p>
+              }
+            </div>
+            <div className="analysis-details">
+              <h3 className="data-smells-title">1) Special Missing Values</h3>
+              <Table
+                col1Arr={analysisData.sp_missing_values.splmissCols}
+                col2Arr={analysisData.sp_missing_values.missingPer}
+                col1={"Column Name"}
+                col2={"Missing Percentage"}
+                smellName={"Special Missing Values"}
+              />
+              {/* <ul className="data-smells-list">
+                {splitIntoSentences(analysisData.sp_missing_values.Info).map(
+                  (sentence, index) => (
+                    <li key={index}>{sentence}</li>
+                  )
+                )}
+              </ul> */}
+              <Codebox
+                language={language}
+                code={analysisData.sp_missing_values.Code}
+              />
+              {bargraph_sp_miss && (
+                <div className="image-box">
+                  <img
+                    src={`data:image/png;base64,${bargraph_sp_miss}`}
+                    alt="special missing values"
+                  />
+                  <div className="fig-text"> Special Missing Values</div>
+                </div>
+              )}
+              {/* <h5> NaN Values</h5>
+              <ul className="data-smells-list">
+                {splitIntoSentences(analysisData.sp_missing_values.InfoNan).map(
+                  (sentence, index) => (
+                    <li key={index}>{sentence}</li>
+                  )
+                )}
+              </ul>
+              <Codebox
+                language={language}
+                code={analysisData.sp_missing_values.Code_Nan}
+              />
+              {bargraph_nan && (
+                <div className="image-box">
+                  <img
+                    src={`data:image/png;base64,${bargraph_nan}`}
+                    alt="NaN values"
+                  />
+                  <div className="fig-text">NaN Values</div>
+                </div>
+              )} */}
+            </div>
+            <div className="analysis-details">
+              <h3 className="data-smells-title"> 2) Missing Values</h3>
+
+              <Table
+                col1Arr={analysisData.missing_values.missCols}
+                col2Arr={analysisData.missing_values.missPer}
+                col1={"Column Name"}
+                col2={"Missing Percentage"}
+                smellName={"Missing Values"}
+              />
+              {/* <ul className="data-smells-list">
+                {splitIntoSentences(analysisData.missing_values.Info).map(
+                  (sentence, index) => (
+                    <li key={index}>{sentence}</li>
+                  )
+                )}
+              </ul> */}
+              <Codebox
+                language={language}
+                code={analysisData.missing_values.Code}
+              />
+              {bargraph_nan && (
+                <div className="image-box">
+                  <img
+                    src={`data:image/png;base64,${bargraph_nan}`}
+                    alt="missing values"
+                  />
+                  <div className="fig-text">Missing Values</div>
+                </div>
+              )}
+            </div>
+            <div className="analysis-details">
+              <h3 className="data-smells-title">3) Binning Categorical</h3>
+              <Table
+                col1Arr={analysisData.binning_cat.binCols}
+                col2Arr={analysisData.binning_cat.unqVals}
+                col1={"Column Name"}
+                col2={"Unique values"}
+                smellName={"Binning Categorical Columns"}
+              />
+              <Codebox
+                language={language}
+                code={analysisData.binning_cat.Code}
+              />
+              {bargraph_binning_cat && (
+                <div className="image-box">
+                  <img
+                    src={`data:image/png;base64,${bargraph_binning_cat}`}
+                    alt="binning_categorical"
+                  />
+                  <div className="fig-text">Binning Categorical</div>
+                </div>
+              )}
+            </div>
+            {/* Class Imbalance */}
+            <div className="analysis-details">
+              <h3 className="data-smells-title">4) Class Imbalance</h3>
+              <Table
+                col1Arr={analysisData.imbalance.imbCols}
+                col2Arr={analysisData.imbalance.imbRatio}
+                col1={"Column Name"}
+                col2={"Imbalance ratio"}
+                smellName={"Categorical Columns with Class Imbalance"}
+              />
+              {/* <ul className="data-smells-list">
+                {splitIntoSentences(analysisData.imbalance.Info).map(
+                  (sentence, index) => (
+                    <li key={index}>{sentence}</li>
+                  )
+                )}
+              </ul> */}
+              <Codebox language={language} code={analysisData.imbalance.Code} />
+              {bargraph_class_imbal && (
+                <div className="image-box">
+                  <img
+                    src={`data:image/png;base64,${bargraph_class_imbal}`}
+                    alt="class_imbalance"
+                  />
+                  <div className="fig-text"> Class Imbalance</div>
+                </div>
+              )}
+            </div>
+            <div className="analysis-details">
+              <h3 className="data-smells-title">5) Special Characters</h3>
+
+              <ul className="data-smells-list">
+                {splitIntoSentences(analysisData.sp_char.Info).map(
+                  (sentence, index) => (
+                    <li key={index}>{sentence}</li>
+                  )
+                )}
+              </ul>
+              <Codebox language={language} code={analysisData.sp_char.Code} />
+              {bargraph_sp_char && (
+                <div className="image-box">
+                  <img
+                    src={`data:image/png;base64,${bargraph_sp_char}`}
+                    alt="special_characters"
+                  />
+                  <div className="fig-text">{"Special Characters"}</div>
+                </div>
+              )}
+            </div>
+            <div className="analysis-details">
+              <h3 className="data-smells-title">{"6) Human Friendly"}</h3>
+              <ul className="data-smells-list">
+                {splitIntoSentences(analysisData.hum_friendly.Info).map(
+                  (sentence, index) => (
+                    <li key={index}>{sentence}</li>
+                  )
+                )}
+              </ul>
+              <Codebox
+                language={language}
+                code={analysisData.hum_friendly.Code}
+              />
+              {bargraph_hum_friendly && (
+                <div className="image-box">
+                  <img
+                    src={`data:image/png;base64,${bargraph_hum_friendly}`}
+                    alt="hum_friendly"
+                  />
+                  <div className="fig-text"> Human Friendly</div>
+                </div>
+              )}
+            </div>
+            <div className="analysis-details">
+              <h3 className="data-smells-title">{"7) Trailing Spaces"}</h3>
+              <ul className="data-smells-list">
+                {splitIntoSentences(analysisData.tr_spaces.Info).map(
+                  (sentence, index) => (
+                    <li key={index}>{sentence}</li>
+                  )
+                )}
+              </ul>
+              <Codebox language={language} code={analysisData.tr_spaces.Code} />
+              {bargraph_tr_spaces && (
+                <div className="image-box">
+                  <img
+                    src={`data:image/png;base64,${bargraph_tr_spaces}`}
+                    alt="tr_spaces"
+                  />
+                  <div className="fig-text">{"8) Trailing Spaces"}</div>
+                </div>
+              )}
+            </div>
+            <div className="analysis-details">
+              <h3 className="data-smells-title">{"9) Correlated Values"}</h3>
+              <div className="heatmap-container">
+                {heatmapData && (
+                  <div className="image-box">
+                    <img
+                      src={`data:image/png;base64,${heatmapData}`}
+                      alt="correlation heatmap"
+                    />
+                    <div className="fig-text">Correlation Heatmap</div>
+                  </div>
+                )}
+                <ul className="data-smells-list">
+                  {analysisData.correlated.map((sentence, index) => (
+                    <li key={index}>{sentence}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="analysis-details">
+              <h3 className="data-smells-title">{"10)Outliers"}</h3>
+              <ul className="data-smells-list">
+                {splitIntoSentences(analysisData.outliers.Info).map(
+                  (sentence, index) => (
+                    <li key={index}>{sentence}</li>
+                  )
+                )}
+              </ul>
+              <Codebox language={language} code={analysisData.outliers.Code} />
+              {boxplot && (
+                <div className="img-box">
+                  <img src={`data:image/png;base64,${boxplot}`} alt="boxplot" />
+                  <div className="fig-text">Fig. Boxplot</div>
+                </div>
+              )}
+            </div>
+            <div className="analysis-details">
+              <h3 className="data-smells-title">{"11) Duplicate Values"}</h3>
+              <ul className="data-smells-list">
+                {splitIntoSentences(analysisData.duplicates).map(
+                  (sentence, index) => (
+                    <li key={index}>{sentence}</li>
+                  )
+                )}
+              </ul>
+            </div>
+            <button className="download-btn" onClick={handleDownload}>
+              Download Results as PDF
+            </button>
+          </div>
+        </div>
+      )}
+      {click && fileChosen && <RegExForm />}
+    </div>
+  );
+}
+
+function splitIntoSentences(text) {
+  const sentences = text.split("\n");
+  return sentences.filter((sentence) => sentence.length > 0);
+}
